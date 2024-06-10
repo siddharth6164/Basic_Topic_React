ead8d44e1ceb77c40a64d53f038748bb87f37cf3
Siddharth Singh <siddhu33singh@gmail.com>
2024-06-10 12:11:36 +0530

    Conditional Rendering

--- src/components/ConditonalRendering/ConditonalRender.jsx
+++ src/components/ConditonalRendering/ConditonalRender.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 function Item({name,isPacked}){
     if(isPacked){
-        return <li style={{color:"green"}}>{name}</li>
+        return <li style={{color:"green"}}>{name} : True </li>
     }
     else{
-        return <li style={{color:"red"}}>{name}</li>
+        return <li style={{color:"red"}}>{name} : False</li>
     }
 }
 
